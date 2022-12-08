a92fd7e66de2f32fc4bb3d35c19415016243139e
1399393088@qq.com <1399393088@qq.com>
2022-12-08 13:58:38 +0800

    add

---
+++ src/main/java/com/czj/dao/cache/RedisDao.java
@@ -0,0 +1,71 @@
+package com.czj.dao.cache;
+
+import com.czj.entity.Seckill;
+import com.dyuproject.protostuff.LinkedBuffer;
+import com.dyuproject.protostuff.ProtostuffIOUtil;
+import com.dyuproject.protostuff.runtime.RuntimeSchema;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+/**
+ * 秒杀 redis
+ */
+
+public class RedisDao {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    private final JedisPool jedisPool;
+
+    public RedisDao(String ip, int port) {
+        jedisPool = new JedisPool(ip, port);
+    }
+
+    // 最高性能序列化
+    private RuntimeSchema<Seckill> seckillRuntimeSchema = RuntimeSchema.createFrom(Seckill.class);
+
+    public Seckill getSeckill(long seckillId) {
+
+        //redis逻辑操作
+        try {
+            Jedis jedis = jedisPool.getResource();
+            try {
+                String key = "seckill:" + seckillId;
+                //采用protostuff序列化对象
+                byte[] bytes = jedis.get(key.getBytes());  // 内部未实现序列化
+                if (bytes != null) {
+                    Seckill seckill = seckillRuntimeSchema.newMessage();  // 空对象
+                    ProtostuffIOUtil.mergeFrom(bytes, seckill, seckillRuntimeSchema);  // 高性能序列化 比jdk自带序列化 性能和空间优秀
+                    //seckill被反序列化
+                    return seckill;
+                }
+            } finally {
+                jedis.close();
+            }
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+        }
+        return null;
+    }
+
+    public String putSeckill(Seckill seckill) {
+        try {
+            Jedis jedis = jedisPool.getResource();
+            try {
+                String key = "seckill:" + seckill.getSeckillId();
+                byte[] bytes = ProtostuffIOUtil.toByteArray(seckill, seckillRuntimeSchema, LinkedBuffer.allocate(LinkedBuffer.DEFAULT_BUFFER_SIZE));
+                //超时缓存 60s
+                int timeout = 60;
+                String result = jedis.setex(key.getBytes(), timeout, bytes);
+                return result;
+            } finally {
+                jedis.close();
+            }
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+        }
+        return null;
+    }
+}
---
+++ src/test/java/com/czj/dao/cache/RedisDaoTest.java
@@ -0,0 +1,41 @@
+package com.czj.dao.cache;
+
+import com.czj.dao.SeckillDao;
+import com.czj.entity.Seckill;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * 测试 redis Dao
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:spring/spring-dao.xml")
+public class RedisDaoTest {
+    private long id = 1001;
+
+    @Autowired
+    private RedisDao redisDao;
+
+    @Autowired
+    private SeckillDao seckillDao;
+
+    @Test
+    public void testSeckill() throws Exception {
+        // redis 获取和添加值
+        Seckill seckill = redisDao.getSeckill(id);
+        if (seckill == null) {
+            seckill = seckillDao.queryById(id);
+            if (seckill != null) {
+                redisDao.putSeckill(seckill);
+                System.out.println("从db中获取秒杀商品信息并put到redis：" + seckill.getName());
+            }
+        } else {
+            System.out.println("直接从redis中获取秒杀商品信息" + seckill.getName());
+        }
+    }
+
+}
